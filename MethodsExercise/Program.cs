060ddb6b55dcf1537327c6a45450e2ad84f85c30
fred47 <Frdrck.hart@gmail.com>
2023-01-28 16:04:35 -0500

    update

--- MethodsExercise/Program.cs
+++ MethodsExercise/Program.cs
@@ -3,8 +3,37 @@
     public class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+
+        { Console.WriteLine ("What is your first name on your birth certificate?");
+          string userName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite color to wear places?");
+              string colorName = Console.ReadLine();
+
+            Console.WriteLine("What is your most enjoyable animal in the world?");
+              string animalName = Console.ReadLine();
+
+            Console.WriteLine("What is your favorite band, you would pay to see?");
+              string bandName = Console.ReadLine();
+
+            
+         Console.WriteLine($" {userName} is his name , his best color to wear is {colorName}, his most enjoyable animal is a {animalName} , he paid to see {bandName} in concert.  ");
+
+
+            
+            
+
+    
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
